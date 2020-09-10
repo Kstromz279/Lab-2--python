9bfb0d697d55be03e30b2315468c23251523b344
Khalil Stroman <7208392@philasd.org>
2020-09-10 00:57:15 +0000

    a lot

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-# Author: Khalil Stroman
+# Author: Khalil Stroman kzs5955@psu.edu
 # Collaborator:
 # Collaborator:
 # Collaborator:
 # Section: 1
 # Breakout: 12
-def getLetterGrade():
-  grade = input("Enter your CMPSC 131 grade: ")
-  grade = float(grade)
+def getLetterGrade(grade):
+  
   if grade >= 93:
-    print("Your letter grade for CMPSC 131 is A.")
+    return "A"
   elif grade >= 90 and grade < 93:
-    print("Your letter grade for CMPSC 131 is A-.")
+    return "A-"
   elif grade >= 87 and grade < 90:
-    print("Your letter grade for CMPSC 131 is B+.")
+    return "B+"
   elif grade >= 83 and grade < 87:
-    print("Your letter grade for CMPSC 131 is B.")
+    return "B"
   elif grade >= 80 and grade < 83:
-    print("Your letter grade for CMPSC 131 is B-.")
+    return "B-"
   elif grade >= 77 and grade < 80:
-    print("Your letter grade for CMPSC 131 is C+.")
+    return "C+"
   elif grade >= 70 and grade < 77:
-    print("Your letter grade for CMPSC 131 is C.")
+    return "C"
   elif grade >= 60 and grade < 70:
-    print("Your letter grade for CMPSC 131 is D.")
+    return "D"
   else:
-    print("Your letter grade for CMPSC 131 is F.")
+    return "F"
+
 
 def run():
-  getLetterGrade()
+  grade = input("Enter your CMPSC 131 grade: ")
+  grade = float(grade)
+  print(f"Your letter grade for CMPSC 131 is{getLetterGrade(grade)}")
+  
 if __name__ == "__main__":
   run()
